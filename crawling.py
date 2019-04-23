8c58684aeff8a4e422ca5102f8db593d248c3a57
Jupiter <devjupiter@gmail.com>
2019-04-23 15:43:32 +0900

    beautifulSoup

--- crawling.py
+++ crawling.py
@@ -1,13 +1,24 @@
 import requests
+from bs4 import BeautifulSoup
 
 ##HTTP GET Request
 req = requests.get('https://beomi.github.io/beomi.github.io_old/')
 
 #html 소스 가져오기
 html = req.text
-##HTTP Header 가져오기
-header = req.headers
-## HTTP Status 가져오기 ( 200: 정상)
-status = req.status_code
-## HTTP가 정상적으로 되었는지 (True/ Falss)
-is_ok = req.ok
+## BeautifulSoup으로 html소스를 python객체로 변환하기
+## 첫 인자는 html소스코드, 두 번째 인자는 어떤 parser를 이용할지 명시.
+## 이 글에서는 Python 내장 html.parser를 이용했다.
+
+soup = BeautifulSoup(html, 'html.parser')
+## string list가 아니라 soup객체의 list이다.
+my_titles = soup.select (
+    'h3 > a'
+)
+
+## my_title는 list 객체
+for title in my_titles:
+    ##Tag안의 텍스트
+    print(title.text)
+    ##Tag의 속성을 가져오기 ex) href
+    print(title.get('href'))
